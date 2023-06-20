a5029e92527c94c3fa825519e89f3a87bb4fe6bf
Equan P <equan.p@gmail.com>
2023-06-20 20:54:38 +0700

    Format

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,16 +1,16 @@
-import { useState } from "react";
-import { Card, Text, Metric } from "@tremor/react";
-import "./App.css";
+import { useState } from 'react'
+import { Card, Text, Metric } from '@tremor/react'
+import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0);
+    const [count, setCount] = useState(0)
 
-  return (
-    <Card className="max-w-xs mx-auto">
-      <Text>Cost</Text>
-      <Metric>$ 34,743</Metric>
-    </Card>
-  );
+    return (
+        <Card className="max-w-xs mx-auto">
+            <Text>Cost</Text>
+            <Metric>$ 34,743</Metric>
+        </Card>
+    )
 }
 
-export default App;
+export default App
