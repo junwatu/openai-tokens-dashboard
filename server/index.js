f532d39e9f4c8794371e2a9a6737590bf26eb5b1
Equan P <equan.p@gmail.com>
2023-06-20 20:54:56 +0700

    Add static folder

--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
 import express from 'express';
+import path from 'node:path';
 import router from './routes.js';
+import { __dirname } from './libs/dirname.js';
 
 const app = express();
 const port = 3000;
+const publicPath = path.resolve(`${__dirname}`, "../ui/dist")
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(publicPath));
 
 app.use('/', router);
 
