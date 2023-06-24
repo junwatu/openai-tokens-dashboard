af099661f238e262e5c4b1df9d58ca6d19bef252
Equan P <equan.p@gmail.com>
2023-06-25 00:24:08 +0700

    Add cors

--- server/index.js
+++ server/index.js
@@ -1,11 +1,18 @@
 import express from 'express';
 import path from 'node:path';
+import cors from 'cors';
+import 'dotenv/config';
+
 import router from './routes.js';
 import { __dirname } from './libs/dirname.js';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const publicPath = path.resolve(`${__dirname}`, '../ui/dist');
+
+app.use(cors({
+	origin: 'http://localhost:5173'
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
