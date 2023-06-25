555a2fcab079977068cceee433aca857c852ed88
Equan P <equan.p@gmail.com>
2023-06-25 16:55:54 +0700

    Add prompt route

--- server/routes.js
+++ server/routes.js
@@ -1,15 +1,18 @@
 import { Router } from 'express';
+import { wordsCountFromPrompt } from './services/prompt.js';
+
 const router = Router();
 
-router.get('/api', (req, res) => {
-	res.json({ message: 'GET route response' });
+router.get('/api', async (req, res) => {
+	const response = await wordsCountFromPrompt({ promptData: null })
+	res.json({ message: 'Tokens usage API', data: response });
 });
 
 router.post('/api', (req, res) => {
 	console.log(req.body);
 	res.json({
 		message: 'POST route response',
-		received: req.body
+		received: req.body,
 	});
 });
 
