9a7a311a10eae6a7506aa27321466deb389e84ba
Equan P <equan.p@gmail.com>
2023-07-04 05:28:55 +0700

    Update the code to handle init and empty input

--- server/routes.js
+++ server/routes.js
@@ -12,9 +12,9 @@ async function saveData({ prompt, cost, details }) {
 	const content = { id, prompt, cost, details };
 	const saveStatus = GridDB.insert(content, collectionDb);
 	if (saveStatus.status) {
-		res.json({ status: saveStatus, operation: 'save' });
+		return { status: saveStatus, operation: 'save' };
 	} else {
-		res.json({ status: saveStatus, operation: 'save' });
+		return { status: saveStatus, operation: 'save' };
 	}
 }
 
@@ -23,13 +23,30 @@ router.post('/api', async (req, res) => {
 	const { input } = req.body;
 
 	if (input === '' || input === null) {
-		// TODO: response to user about empty input
-		const response = await wordsCountFromPrompt({ promptData: null });
-		res.json({ message: 'Tokens usage API', data: response });
+		/**
+		 * rsponse sample output
+		 * {
+			id: 'chatcmpl-7YMGXEuuR4Wi7HKNFa5XAbTL5hWgb',
+			object: 'chat.completion',
+			created: 1688423097,
+			model: 'gpt-3.5-turbo-16k-0613',
+			choices: [ { index: 0, message: [Object], finish_reason: 'stop' } ],
+			usage: { prompt_tokens: 148, completion_tokens: 6, total_tokens: 154 }
+			}
+		 */
+		const response = {
+			id: 'chatcmpl-noid',
+			object: 'chat.completion',
+			created: new Date(),
+			model: '',
+			choices: [{ index: 0, message: '', finish_reason: 'stop' }],
+			usage: { prompt_tokens: 0, completion_tokens: 0, total_tokens: 0 }
+		}
+		res.json({ message: 'No tokens usage', data: response });
 	} else {
 		// TODO: save data
 		const response = await wordsCountFromPrompt({ promptData: input });
-		res.json({ message: 'Tokens usage API', data: response });
+		res.json({ message: 'Data tokens usage ', data: response });
 	}
 });
 
